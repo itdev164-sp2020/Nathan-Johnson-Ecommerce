e859ab9f95906958768b4c7a066354ae6885c529
Nathan Johnson <nathanjohnson@Nathans-MBP.lan>
2020-04-27 12:27:58 -0500

    vghghgh

---
+++ gatsby-starter-ecommerce/src/pages/__tests__/register.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react'
+import {render} from '@testing-library/react'
+import Register from '../register'
+
+const props = {location: {pathname: ''}}
+
+test('renders', () => {
+  const {asFragment} = render(<Register location={{}} />)
+  expect(asFragment()).toMatchSnapshot()
+})
+
+test('register renders name and email', () => {
+  const {getByLabelText} = render(<Register {...props} />)
+  expect(getByLabelText(/Email/i))
+  expect(getByLabelText(/name/i))
+})
